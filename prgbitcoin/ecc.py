8075513d3f0ac520c982153bfcf88db07a5b1297
Boseong Choi <cbscsm@gmail.com>
2022-05-28 21:53:10 +0900

    Add point source code

--- prgbitcoin/ecc.py
+++ prgbitcoin/ecc.py
@@ -103,3 +103,91 @@ class FieldElementTest(TestCase):
         a = FieldElement(4, 31)
         b = FieldElement(11, 31)
         self.assertEqual(a**-4 * b, FieldElement(13, 31))
+
+
+# tag::source1[]
+class Point:
+
+    def __init__(self, x, y, a, b):
+        self.a = a
+        self.b = b
+        self.x = x
+        self.y = y
+        # end::source1[]
+        # tag::source2[]
+        if self.x is None and self.y is None:  # <1>
+            return
+        # end::source2[]
+        # tag::source1[]
+        if self.y**2 != self.x**3 + a * x + b:  # <1>
+            raise ValueError('({}, {}) is not on the curve'.format(x, y))
+
+    def __eq__(self, other):  # <2>
+        return self.x == other.x and self.y == other.y \
+            and self.a == other.a and self.b == other.b
+    # end::source1[]
+
+    def __ne__(self, other):
+        # this should be the inverse of the == operator
+        raise NotImplementedError
+
+    def __repr__(self):
+        if self.x is None:
+            return 'Point(infinity)'
+        else:
+            return 'Point({},{})_{}_{}'.format(self.x, self.y, self.a, self.b)
+
+    # tag::source3[]
+    def __add__(self, other):  # <2>
+        if self.a != other.a or self.b != other.b:
+            raise TypeError('Points {}, {} are not on the same curve'.format
+            (self, other))
+
+        if self.x is None:  # <3>
+            return other
+        if other.x is None:  # <4>
+            return self
+        # end::source3[]
+
+        # Case 1: self.x == other.x, self.y != other.y
+        # Result is point at infinity
+
+        # Case 2: self.x ≠ other.x
+        # Formula (x3,y3)==(x1,y1)+(x2,y2)
+        # s=(y2-y1)/(x2-x1)
+        # x3=s**2-x1-x2
+        # y3=s*(x1-x3)-y1
+
+        # Case 3: self == other
+        # Formula (x3,y3)=(x1,y1)+(x1,y1)
+        # s=(3*x1**2+a)/(2*y1)
+        # x3=s**2-2*x1
+        # y3=s*(x1-x3)-y1
+
+        raise NotImplementedError
+
+
+class PointTest(TestCase):
+
+    def test_ne(self):
+        a = Point(x=3, y=-7, a=5, b=7)
+        b = Point(x=18, y=77, a=5, b=7)
+        self.assertTrue(a != b)
+        self.assertFalse(a != a)
+
+    def test_add0(self):
+        a = Point(x=None, y=None, a=5, b=7)
+        b = Point(x=2, y=5, a=5, b=7)
+        c = Point(x=2, y=-5, a=5, b=7)
+        self.assertEqual(a + b, b)
+        self.assertEqual(b + a, b)
+        self.assertEqual(b + c, a)
+
+    def test_add1(self):
+        a = Point(x=3, y=7, a=5, b=7)
+        b = Point(x=-1, y=-1, a=5, b=7)
+        self.assertEqual(a + b, Point(x=2, y=-5, a=5, b=7))
+
+    def test_add2(self):
+        a = Point(x=-1, y=-1, a=5, b=7)
+        self.assertEqual(a + a, Point(x=18, y=77, a=5, b=7))
