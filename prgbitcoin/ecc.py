86418a46e18d1ccb08dbcff701831193902a9b5b
Boseong Choi <cbscsm@gmail.com>
2022-05-29 19:54:25 +0900

    Implemente Point

--- prgbitcoin/ecc.py
+++ prgbitcoin/ecc.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from unittest import TestCase
 
 from typing_extensions import Self
@@ -105,66 +106,60 @@ class FieldElementTest(TestCase):
         self.assertEqual(a**-4 * b, FieldElement(13, 31))
 
 
-# tag::source1[]
 class Point:
-
-    def __init__(self, x, y, a, b):
+    def __init__(self, x: Optional[float], y: Optional[float], a: float, b: float):
+        if x is None or y is None:
+            if x != y:
+                raise ValueError(f'x와 y가 둘 중 하나만 None일 수 없습니다.')
+        elif y**2 != x**3 + a * x + b:
+            raise ValueError(f'({x}, {y}) is not on the curve')
         self.a = a
         self.b = b
         self.x = x
         self.y = y
-        # end::source1[]
-        # tag::source2[]
-        if self.x is None and self.y is None:  # <1>
-            return
-        # end::source2[]
-        # tag::source1[]
-        if self.y**2 != self.x**3 + a * x + b:  # <1>
-            raise ValueError('({}, {}) is not on the curve'.format(x, y))
 
-    def __eq__(self, other):  # <2>
-        return self.x == other.x and self.y == other.y \
-            and self.a == other.a and self.b == other.b
-    # end::source1[]
-
-    def __ne__(self, other):
-        # this should be the inverse of the == operator
-        raise NotImplementedError
+    def __eq__(self, other: object):
+        if not isinstance(other, Point):
+            return False
+        return (
+            self.x == other.x
+            and self.y == other.y
+            and self.a == other.a
+            and self.b == other.b
+        )
 
     def __repr__(self):
-        if self.x is None:
-            return 'Point(infinity)'
-        else:
-            return 'Point({},{})_{}_{}'.format(self.x, self.y, self.a, self.b)
+        return f'Point({self.x}, {self.y}, {self.a}, {self.b})'
 
-    # tag::source3[]
-    def __add__(self, other):  # <2>
-        if self.a != other.a or self.b != other.b:
-            raise TypeError('Points {}, {} are not on the same curve'.format
-            (self, other))
+    def __add__(self, other: 'Point') -> Self:
+        if isinstance(other, Point):
+            if self.a != other.a or self.b != other.b:
+                raise TypeError(f'Points {self!r}, {other!r} are not on the same curve')
+            if self.is_at_infinite():
+                return other
+            if other.is_at_infinite():
+                return self
+            if self.x == other.x:
+                if self.y != other.y or self.y == other.y == 0:
+                    return self.new_at_infinite()
+                s = (3 * self.x**2 + self.a) / (2 * self.y)
+                x3 = s**2 - 2 * self.x
+            else:
+                s = (other.y - self.y) / (other.x - self.x)
+                x3 = s**2 - self.x - other.x
+            return self.__class__(
+                x3,
+                s * (self.x - x3) - self.y,
+                self.a,
+                self.b,
+            )
+        return NotImplemented
 
-        if self.x is None:  # <3>
-            return other
-        if other.x is None:  # <4>
-            return self
-        # end::source3[]
+    def new_at_infinite(self) -> Self:
+        return self.__class__(None, None, self.a, self.b)
 
-        # Case 1: self.x == other.x, self.y != other.y
-        # Result is point at infinity
-
-        # Case 2: self.x ≠ other.x
-        # Formula (x3,y3)==(x1,y1)+(x2,y2)
-        # s=(y2-y1)/(x2-x1)
-        # x3=s**2-x1-x2
-        # y3=s*(x1-x3)-y1
-
-        # Case 3: self == other
-        # Formula (x3,y3)=(x1,y1)+(x1,y1)
-        # s=(3*x1**2+a)/(2*y1)
-        # x3=s**2-2*x1
-        # y3=s*(x1-x3)-y1
-
-        raise NotImplementedError
+    def is_at_infinite(self) -> bool:
+        return self.x is None or self.y is None
 
 
 class PointTest(TestCase):
